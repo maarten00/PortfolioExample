097f9b98ee3603972fff94c65172951d8dd698c7
Maarten Kuiper <maarten00@gmail.com>
2012-06-06 01:39:55 +0200

    Cleaned up file

--- backend/dbConnect.php
+++ backend/dbConnect.php
@@ -5,12 +5,6 @@ $password=""; // Mysql password
 $db_name="portfolio"; // Database name 
 $tbl_name="examples"; // Table name
 
-// Connect to server and select databse.
-/*
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
- */
-
 $mysqli = new mysqli($host, $username, $password, $db_name);
 if($mysqli -> connect_errno) {
 echo "Unable to connect: " . $mysqli -> connect_errno;
