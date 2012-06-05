2f5930dd4f88dc1af8c24be44399a1bca58ca189
Maarten Kuiper <maarten00@gmail.com>
2012-06-06 01:37:32 +0200

    Cleaned up with seperate headers file

--- index.php
+++ index.php
@@ -7,24 +7,10 @@ if (isset($_SESSION["user"])) {
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8" />
-
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
 		<title>Portfolio Example</title>
-		<meta name="description" content="Portfolio example" />
-		<meta name="author" content="Maarten" />
-
-		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
-
-		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-		<link rel="shortcut icon" href="img/favicon.ico" />
-		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-		<link rel="stylesheet" href="style/main.css" />
-		<script src="scripts/functions.js"></script>
-		<script src="libs/jquery-1.7.1.js"></script>
+		<?PHP
+			include 'backend/headers.xhtml';
+		?>
 	</head>
 
 	<body onload="attachHandlers()">
@@ -39,18 +25,27 @@ if (isset($_SESSION["user"])) {
 			<h1>Portfolio Example</h1>
 		</header>
 		<nav>
-			<a href="/">Home</a>
-			
-			<a href="/contact">Contact</a>
+			<a href="">Home</a>
+			<a href="contact">Contact</a>
 		</nav>
+		<?PHP
+			if (checkLogin()) {
+				echo '<a href="" class="btn add">Add</a>';
+			}
+		?>
 		<section class='examples'>
 			Hier komen de voorbeelden van gemaakte websites.
 			<br />
 			<a id="getAllExamples">Haal voorbeelden op</a>
+			<?PHP
+				if (checkLogin()) {
+
+				}
+			?>
 		</section>
 		<footer>
 			<p>
-				&copy; Copyright  by Maarten
+				&copy; Copyright  by Maarten Kuiper
 			</p>
 		</footer>
 		</div>
