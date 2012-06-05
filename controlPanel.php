8b0109d48ea9b6234961c690dfb37b27bc587c58
Maarten Kuiper <maarten00@gmail.com>
2012-06-06 01:37:13 +0200

    Cleaned up with seperate headers file

--- controlPanel.php
+++ controlPanel.php
@@ -7,24 +7,10 @@ if (isset($_SESSION["user"])) {
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8" />
-
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-		<title>Portfolio Example</title>
-		<meta name="description" content="Portfolio example" />
-		<meta name="author" content="Maarten" />
-
-		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
-
-		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-		<link rel="shortcut icon" href="img/favicon.ico" />
-		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-		<link rel="stylesheet" href="style/main.css" />
-		<script src="scripts/functions.js"></script>
-		<script src="libs/jquery-1.7.1.js"></script>
+		<title>Portfolio Example - CP</title>
+		<?PHP
+	include 'backend/headers.xhtml';
+		?>
 	</head>
 
 	<body onload="attachHandlers()">
@@ -36,19 +22,19 @@ if (isset($_SESSION["user"])) {
 		}
 		?>
 		<header>
-			<h1>Portfolio Example</h1>
+			<h1>Portfolio Example - CP</h1>
 		</header>
-		<nav>
-			<a href="/">Home</a>
-		</nav>
-		<div class='login-form'>
-			<h1>Login Form</h1>
+		<nav></nav>
+		<section class='loginForm'>
+			<header>
+				<h1>Login Form</h1>
+			</header>
 			<form action="backend/checkcredentials.php" method="post">
-				<input type="text" name="username" placeholder="username">
-				<input type="password" name="password" placeholder="password">
+				<input type="text" name="username" placeholder="Username">
+				<input type="password" name="password" placeholder="Password">
 				<input type="submit" value="Login">
 			</form>
-		</div>
+		</section>
 		<footer>
 			<p>
 				&copy; Copyright  by Maarten
