29cff0e254e7a74b3750822a242752a45decd6a9
Maarten Kuiper <maarten00@gmail.com>
2012-07-01 14:32:33 +0200

    Added method to allow logout via an XHR request

--- backend/logout.php
+++ backend/logout.php
@@ -2,4 +2,10 @@
 session_start();
 session_destroy();
 header("location:../index.php");
+
+if (isset($_REQUEST["method"]) && $_REQUEST["method"] == "endSession") {
+	session_start();
+	session_destroy();
+	header("location:../index.php");
+}
 ?>
